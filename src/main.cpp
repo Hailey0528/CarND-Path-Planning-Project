5385ba45fa634a6e1c41728f7d4fd23cb6d81799
Hailey0528 <huanhuanxiong0@gmail.com>
2019-02-14 22:33:29 +0100

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -244,6 +244,8 @@ int main() {
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
+                // Some code are from the course of Udacity
+
                 int prev_size = previous_path_x.size();
                 if(prev_size > 0)
                 {
